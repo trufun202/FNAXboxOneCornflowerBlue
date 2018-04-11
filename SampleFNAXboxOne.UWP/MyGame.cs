bee9677e77be7d30c320e5a4a8e57f1628be9c43
unknown <chris.bryant@FA00036.corp.group1200.com>
2018-04-10 20:34:57 -0500

    Initial commit

---
+++ SampleFNAXboxOne.UWP/MyGame.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Xml;
+using System.IO;
+using System.Reflection;
+
+namespace SampleFNAXboxOne.UWP
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class MyGame : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public MyGame()
+        {
+            Window.Title = "Sample FNA Xbox One Game";
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void OnActivated(object sender, EventArgs args)
+        {
+            //if the game loses focus, we have to rebuild the pre-rendered map
+            base.OnActivated(sender, args);
+        }
+
+        protected override void OnDeactivated(object sender, EventArgs args)
+        {
+            base.OnDeactivated(sender, args);
+        }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            InitGraphicsMode(1280, 720, false);
+            base.Initialize();
+        }
+
+        private bool InitGraphicsMode(int iWidth, int iHeight, bool bFullScreen)
+        {
+            // If we aren't using a full screen mode, the height and width of the window can
+            // be set to anything equal to or smaller than the actual screen size.
+            if (bFullScreen == false)
+            {
+                graphics.PreferredBackBufferWidth = iWidth;
+                graphics.PreferredBackBufferHeight = iHeight;
+                graphics.IsFullScreen = bFullScreen;
+                graphics.ApplyChanges();
+                return true;
+                //}
+            }
+            else
+            {
+                // If we are using full screen mode, we should check to make sure that the display
+                // adapter can handle the video mode we are trying to set.  To do this, we will
+                // iterate thorugh the display modes supported by the adapter and check them against
+                // the mode we want to set.
+                foreach (DisplayMode dm in GraphicsAdapter.DefaultAdapter.SupportedDisplayModes)
+                {
+                    //// Check the width and height of each mode against the passed values
+                    //if ((dm.Width == iWidth) && (dm.Height == iHeight))
+                    //{
+                    // The mode is supported, so set the buffer formats, apply changes and return
+                    graphics.PreferredBackBufferWidth = iWidth;
+                    graphics.PreferredBackBufferHeight = iHeight;
+                    graphics.IsFullScreen = bFullScreen;
+                    graphics.ApplyChanges();
+                    return true;
+                    //}
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per gam-e and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                this.Exit();
+            }
+
+            base.Update(gameTime);
+        }
+        
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+             base.Draw(gameTime);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ SampleFNAXboxOne.UWP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFNAXboxOne.UWP
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+#if WINDOWS_UWP
+        {
+            // Temporary workaround until FACT is done
+            //Environment.SetEnvironmentVariable("FNA_AUDIO_DISABLE_SOUND", "1");
+
+            realArgs = args;
+            SDL2.SDL.SDL_SetHint("SDL_WINRT_HANDLE_BACK_BUTTON", "1");
+            SDL2.SDL.SDL_WinRT_mainFunction mainFunction = FakeMain;
+            SDL2.SDL.SDL_WinRTRunApp(mainFunction, IntPtr.Zero);
+        }
+
+        static string[] realArgs;
+        static int FakeMain(int argc, IntPtr[] argv)
+        {
+            RealMain(realArgs);
+            return 0;
+        }
+
+        static void RealMain(string[] args)
+#endif
+        {
+            using (MyGame game = new MyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
